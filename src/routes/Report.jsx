12601f34d730e3b6378697071ba77e7d4bf6c95a
rmena1 <rma@uc.cl>
2022-07-08 19:22:04 -0400

    fix: graph header

--- src/routes/Report.jsx
+++ src/routes/Report.jsx
@@ -7,7 +7,7 @@ const Report = () => {
     const { date_from, date_to } = useParams()
 
     return (
-        <div className='w-5/6 pt-10 mx-auto'>
+        <div className='w-5/6 pt-20 mx-auto'>
             <h1 className="font-medium leading-tight text-xl mt-0 text-sky-800 mb-10">Reporte de ventas realizadas entre {date_from} y {date_to}</h1>
             <CategoryProductSales dateFrom={date_from} dateTo={date_to} />
             <SalesGraphs dateFrom={date_from} dateTo={date_to} />
